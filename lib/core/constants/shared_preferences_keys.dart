6e51d469df8f548499b97a2e9f14af1b70fa4120
VitorFranciscoDev <vitorfndr@gmail.com>
2025-11-18 19:56:13 -0300

    feat: constant keys

--- lib/core/constants/shared_preferences_keys.dart
+++ lib/core/constants/shared_preferences_keys.dart
@@ -1,3 +1,10 @@
 class SharedPreferencesKeys {
+  /// The key for User's Shared Preferences Storage
   static const String userStorageKey = "user";
+
+  /// The key for Theme's Shared Preferences Storage
+  static const String themeStorageKey = "theme";
+
+  /// The key for Intl's Shared Preferences Storage
+  static const String intlStorageKey = "intl";
 }
